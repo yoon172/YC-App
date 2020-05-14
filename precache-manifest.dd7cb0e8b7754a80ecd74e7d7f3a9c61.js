e78c42a00744253ec4a86687207189ba97a0542c
Yoon Cho <yoon172@gmail.com>
2020-05-14 18:43:37 -0500

    Updates

---
+++ precache-manifest.dd7cb0e8b7754a80ecd74e7d7f3a9c61.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5008008036684fbae8a8bc5f66ab8261",
+    "url": "/YC-App/index.html"
+  },
+  {
+    "revision": "4600b3099e04dfd819d8",
+    "url": "/YC-App/static/css/main.9e846746.chunk.css"
+  },
+  {
+    "revision": "d754278b6c1be87ec2e6",
+    "url": "/YC-App/static/js/2.c6077cf0.chunk.js"
+  },
+  {
+    "revision": "90377550dd86ed303dd30d8f487872de",
+    "url": "/YC-App/static/js/2.c6077cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4600b3099e04dfd819d8",
+    "url": "/YC-App/static/js/main.9b955e8e.chunk.js"
+  },
+  {
+    "revision": "312b86d7cba4696493ab",
+    "url": "/YC-App/static/js/runtime-main.3233149b.js"
+  },
+  {
+    "revision": "2bff61c9eb54a9b19c7164fc565280d6",
+    "url": "/YC-App/static/media/ProfilePicture3.2bff61c9.jpg"
+  },
+  {
+    "revision": "63d66d3d312f76da5dddb88ec20a3626",
+    "url": "/YC-App/static/media/mountain.63d66d3d.jpeg"
+  }
+]);
